459e6c9c717965786e540f23e3853ce93b8ab27e
Thomas Andrews <github@thomasoandrews.com>
2023-05-04 14:34:41 -0400

    Commit 5a53d8b098a669a62de3e452ba8a6c8333553825 to https://github.com/thomasoa/IBBJS.git

--- impossible/numeric/modinverse.js
+++ impossible/numeric/modinverse.js
@@ -1,11 +1,18 @@
+var zero = BigInt(0);
+var one = BigInt(1);
+function abs(value) {
+    if (value < BigInt(0)) {
+        return -value;
+    }
+    else {
+        return value;
+    }
+}
 export function safe_mod(n1, n2) {
     // Computes n1 % n2, but with values r in range
     // 0 <= r < abs(n2)
     // The % operator sometimes returns negative numbers
-    var zero = BigInt(0);
-    if (n2 < zero) {
-        n2 = -n2;
-    }
+    n2 = abs(n2);
     var result = n1 % n2;
     if (result < zero) {
         return result + n2;
@@ -16,57 +23,48 @@ export function safe_mod(n1, n2) {
 }
 export function long_gcd(m, n) {
     var quotients = Array();
-    var zero = BigInt(0);
-    if (m < zero) {
-        m = -m;
-    }
+    m = abs(m);
     if (m == zero) {
         throw Error('long_gcd cannot be called when m is zero');
     }
     n = safe_mod(n, m);
-    if (n == zero) {
-        return { gcd: m, quotients: quotients };
-    }
     while (n != zero) {
         var q = m / n;
         var r = m % n;
-        if (r == zero) {
-            return { gcd: n, quotients: quotients };
+        if (r != zero) {
+            quotients.push(q);
         }
-        quotients.push(q);
         m = n;
         n = r;
     }
-    throw new Error("Impossible condition");
+    return { gcd: m, quotients: quotients };
+}
+function buildInverse(quotients, modulus) {
+    // Standard algorithm for contiued fraction expansion
+    // Numerators only needed.
+    var p_0 = zero, p_1 = one;
+    quotients.forEach(function (quotient) {
+        var p_new = p_1 * quotient + p_0;
+        p_0 = p_1;
+        p_1 = p_new;
+    });
+    if (quotients.length % 2 == 0) {
+        return p_1;
+    }
+    else {
+        return modulus - p_1;
+    }
 }
 export function modular_inverse(modulus, unit) {
-    var zero = BigInt(0), one = BigInt(1);
     if (modulus < zero) {
         modulus = -modulus;
     }
     unit = safe_mod(unit, modulus);
-    if (unit == zero) {
-        throw Error('Unit ' + unit + ' is divible by the modulus ' + modulus);
-    }
     var result = long_gcd(modulus, unit);
     if (result.gcd != one) {
         throw Error('Modulus ' + modulus
             + ' and unit ' + unit
             + ' are not relatively prime, gcd=' + result.gcd);
     }
-    var p_0 = zero, p_1 = one, q_0 = one, q_1 = zero;
-    result.quotients.forEach(function (quotient) {
-        var p_new = p_1 * quotient + p_0;
-        var q_new = q_1 * quotient + q_0;
-        p_0 = p_1;
-        p_1 = p_new;
-        q_0 = q_1;
-        q_1 = q_new;
-    });
-    if (result.quotients.length % 2 == 0) {
-        return p_1;
-    }
-    else {
-        return modulus - p_1;
-    }
+    return buildInverse(result.quotients, modulus);
 }
