2d125e49b253ac3819702f0b7d88d223aace7d8e
Thomas Andrews <github@thomasoandrews.com>
2023-05-10 11:32:44 -0400

    Remove unbundled js files

--- impossible/bridge/index.js
+++
@@ -1,4 +0,0 @@
-import { Seats, Deck, Rank, Card } from "../basics/src/bridge/constants.js";
-import { BridgeBook, SimpleBijection } from "./book.js";
-import { Deal, Hand, Holding } from "./deal.js";
-export { Seats, Deck, Rank, Card, BridgeBook, SimpleBijection, Deal, Hand, Holding };
